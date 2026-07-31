5def074c20fcb3dc8712cdd5738cac7372260a46
thanawat150 <thanawat.ratc@gmail.com>
2026-07-31 09:03:30 +0700

    Make Visual Automation the primary startup experience

--- src/AutoCutStudio.App/App.xaml.cs
+++ src/AutoCutStudio.App/App.xaml.cs
@@ -1,56 +1,25 @@
-using System.IO;
 using System.Windows;
-using System.Windows.Threading;
 
 namespace AutoCutStudio.App;
 
 public partial class App : Application
 {
-    public App()
-    {
-        DispatcherUnhandledException += OnDispatcherUnhandledException;
-    }
-
     protected override void OnStartup(StartupEventArgs e)
     {
+        AppPaths.EnsureCreated();
         base.OnStartup(e);
-        Dispatcher.BeginInvoke(
-            DispatcherPriority.Loaded,
-            new Action(() =>
+        Dispatcher.BeginInvoke(() =>
+        {
+            if (MainWindow is MainWindow window)
             {
-                if (Current.MainWindow is AutoCutStudio.App.MainWindow window)
-                {
-                    window.EnableAutomaticEditingUi();
-                    window.EnableSpeechEditingUi();
-                    window.EnableSocialAutomationUi();
-                    window.EnableEnhancementUi();
-                    window.EnableAdvancedAiUi();
-                    window.EnableProfessionalToolsUi();
-                }
-            }));
-    }
-
-    private static void OnDispatcherUnhandledException(
-        object sender,
-        DispatcherUnhandledExceptionEventArgs eventArgs)
-    {
-        try
-        {
-            Directory.CreateDirectory(AppPaths.LogDirectory);
-            File.AppendAllText(
-                Path.Combine(AppPaths.LogDirectory, "app-crash.log"),
-                $"[{DateTimeOffset.UtcNow:O}]{Environment.NewLine}{eventArgs.Exception}{Environment.NewLine}{Environment.NewLine}");
-        }
-        catch
-        {
-            // Do not hide the original exception if crash logging fails.
-        }
-
-        MessageBox.Show(
-            $"เกิดข้อผิดพลาดที่ไม่ได้คาดไว้\n\n{eventArgs.Exception.Message}\n\nSource Media ไม่ถูกแก้ไข",
-            "AutoCut Studio",
-            MessageBoxButton.OK,
-            MessageBoxImage.Error);
-        eventArgs.Handled = true;
+                window.EnableVisualAutomationUi();
+                window.EnableAutomaticEditingUi();
+                window.EnableSpeechEditingUi();
+                window.EnableSocialAutomationUi();
+                window.EnableEnhancementUi();
+                window.EnableAdvancedAiUi();
+                window.EnableProfessionalToolsUi();
+            }
+        });
     }
 }
